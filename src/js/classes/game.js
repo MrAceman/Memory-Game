3f9e25b9b4d1f57ed2c96ddcaf598ca45d4d7141
Niclas Willems <niclas.willems@gmail.com>
2016-07-16 15:43:04 -0400

    Taking a break from functionality and playing with styling.

--- src/js/classes/game.js
+++ src/js/classes/game.js
@@ -3,17 +3,17 @@ import {Card} from './card.js';
 
 class Game{
   constructor(){
-    this.deck = ["../images/blaze.jpg", "../images/blaze.jpg",
-    "../images/aj.jpg", "../images/aj.jpg",
-    "../images/blazelogo.png", "../images/blazelogo.png",
-    "../images/crusher.jpg", "../images/crusher.jpg",
-    "../images/darington.png", "../images/darington.png",
-    "../images/gabby.png", "../images/gabby.png",
-    "../images/pickle.jpg", "../images/pickle.jpg",
-    "../images/starla.png", "../images/starla.png",
-    "../images/stripes.png", "../images/stripes.png",
-    "../images/zeg.png", "../images/zeg.png"];
-    // this.deck = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
+    // this.deck = ["../images/blaze.jpg", "../images/blaze.jpg",
+    // "../images/aj.jpg", "../images/aj.jpg",
+    // "../images/blazelogo.png", "../images/blazelogo.png",
+    // "../images/crusher.jpg", "../images/crusher.jpg",
+    // "../images/darington.png", "../images/darington.png",
+    // "../images/gabby.png", "../images/gabby.png",
+    // "../images/pickle.jpg", "../images/pickle.jpg",
+    // "../images/starla.png", "../images/starla.png",
+    // "../images/stripes.png", "../images/stripes.png",
+    // "../images/zeg.png", "../images/zeg.png"];
+    this.deck = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
     this.previousCard = 0;
     this.valCard = 0;
     this.cardTarget;
@@ -39,8 +39,8 @@ class Game{
       let i, card, x;
       let d = shuffledDeck;
       for (i = d.length; i; i--) {
-        // node.append(`<li class="card facedown" id="${i}">${d[i-1]}</li>`);
-        node.append(`<li class="card facedown" id="${i}"><img src="${this.deck[i-1]}" width=85%></li>`);
+        node.append(`<li class="card facedown" id="${i}">${d[i-1]}</li>`);
+        // node.append(`<li class="card facedown" id="${i}"><img src="${this.deck[i-1]}" width=85%></li>`);
 
       }
     }
