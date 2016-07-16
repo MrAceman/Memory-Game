01de1ec4d0dbf2c954b3f4f3fc58615cbfa83833
Niclas Willems <niclas.willems@gmail.com>
2016-07-15 22:51:31 -0400

    Working on functionality to flip cards back over. JS/DOM/CSS interactivity can be fun.

--- src/js/main.js
+++ src/js/main.js
@@ -20,12 +20,7 @@ function checkMatches(){
   if (newGame.valCard === newGame.previousCard){
     console.log("Inside if 1 - match");
     console.log(newGame.deck);
-    for (var i = newGame.deck.length-1; i--;){
-	     if (newGame.deck[i] === Number(newGame.valCard)) newGame.deck.splice(i, 1);
-       newGame.deck = newGame.deck;
-     }
-  newGame.valCard.each(function(){console.log(this)});
-  // $('.card').each(function(){console.log(this)});
+    removeMatches();
     newGame.valCard = 0;
   }
   else if (newGame.previousCard === 0){
@@ -49,22 +44,70 @@ function checkMatches(){
     console.log(newGame.valCard);
 }
 
-// function removeMatches(card){
-//
-// }
+function removeMatches(){
 
-// function coverCards(){
-//   $('.card').each(function(){console.log(this)});
-// }
+  for (var i = newGame.deck.length-1; i--;){
+     if (newGame.deck[i] === Number(newGame.valCard)) newGame.deck.splice(i, 1);
+     newGame.deck = newGame.deck;
+   }
+   coverCards();
+ };
+// newGame.valCard.each(function(){console.log(this)});
+// $('.card').each(function(){
+//   var currentCard = this;
+// });
+// $('.card').each(function(){
+  // var currentCard = this;
+// });
+
+  // console.log($('.card'));
+  // var thisCard = .card;
+  // newGame.deck.map(function(o,i){
+  //       if (currentCard.innerHTML == newGame.deck[i]){
+  //           console.log(currentCard.innerHTML);
+  //       console.log(newGame.deck[i])
+  //       console.log($(currentCard.className));
+  //       $(currentCard.className).addClass('facedown');
+  //
+  //     }
+  // })
+
+
+function coverCards(){
+  var allCards = $('.card');
+  let i, o; //Counters for both array loops
+
+  console.log(allCards);
+
+// Need to figure out how to loop through the cards on the pages... $('.card')
+// and also the newGame.deck (which only contains unmatched cards).
+// If the .card.innerHTML[i] matches the newGame.deck[o] then add the CSS class facedown
+
+    allCards.forEach(function(){
+});
+
+    // newGame.deck.each(function(o){
+      // if {allCards[i] === newGame.deck[o]}
+      // if (allCards[i].innerHTML == newGame.deck[o]){
+      // curID = allCards[i].id;
+      // $(`"#${curID}"`).addClass('facedown');
+      // $("#allCards[i].id").addClass('facedown');
+      // $('card')[i].class('facedown');
+    // }
+    // });
+  // })
+  // for (var i = newGame.deck.length-1; i--){
+  //   if (newGame.deck[i] === allCards.innerHTML)
+  // }
+  // $('.card').each(function(){console.log(this)});
+}
 
 var changeCardState = function(e) {
   event.preventDefault();
-  // console.log(e);
   let thisCard = e.currentTarget;
   let thisCardClass = e.currentTarget.className;
 
   if (thisCardClass.indexOf('facedown')){
-    // console.log(this);
     $(this).removeClass('facedown');
   };
   newGame.valCard = e.currentTarget.innerHTML;
